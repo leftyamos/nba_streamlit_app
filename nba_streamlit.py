d5480b799b94de80e12704ea0409923544f6901f
leftyamos <63567814+leftyamos@users.noreply.github.com>
2024-04-30 17:10:41 +0800

    Update nba_streamlit.py
    - rearrange code position.
    - "st.beta_columns" is an older version of Streamlit method that is not supported, use "st.columns" instead.

--- nba_streamlit.py
+++ nba_streamlit.py
@@ -1,5 +1,8 @@
 # Made by Amos Tai
 # Date: 03/02/2021
+# Updated: 30/04/2024
+
+# Update notes: rearrange code and use new version of Streamlit method.
 
 # The goal for this project is to determine if the modern NBA starting from 2010-2011 season 
 # until 2019-2020 season is truely positionless and if it isn't, can a player be classified 
@@ -19,6 +22,141 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVC
 from sklearn.pipeline import make_pipeline
 
+
+@st.cache(allow_output_mutation=True)        
+def read_multi(file_list):
+    '''
+    take a list of files concat it together.
+    
+    '''
+    list_df = []
+    
+    for file in (file_list):
+        df = pd.read_csv(file, sep=',', header=0)
+        
+        df['Sea'] = file.split('.')[0]
+        
+        list_df.append(df)
+    
+    total_df = pd.concat(list_df, axis=0)
+    
+    return total_df
+
+
+
+def run_eda(df):
+    '''
+    Visualize data
+    '''
+    years = ['2010-2011', '2011-2012', '2012-2013', '2013-2014', '2014-2015',
+             '2015-2016', '2016-2017', '2017-2018', '2018-2019', '2019-2020']
+    
+    seasons = st.selectbox('Select season', years)
+    
+    fig = plt.figure(figsize=(10,5))
+    filter_season = df[df['Sea']==seasons]['Pos'].value_counts().sort_index().loc[['PG','SG','SF','PF','C']].plot(kind='bar')
+    plt.ylim(0,140)
+    plt.xlabel('Positions')
+    plt.ylabel('Players')
+    plt.title(f'Total no. of players during {seasons} season')
+    st.write(fig)
+    
+    row1_1, row1_2 = st.columns((1,1))
+    with row1_1:
+        features = st.selectbox('Select feature', df.drop(['Rk','Pos','Player','Tm','GS','Sea'], axis=1).columns)
+        
+    with row1_2:
+        positions = st.selectbox('Select position', ['PG','SG','SF','PF','C'])
+    
+    fig = plt.figure(figsize=(3,3))
+    filter_fea_pos = sns.histplot(data=df[(df['Sea']==seasons) & (df['Pos']==positions)], x=features)
+    plt.title(f'Histogram \n{seasons}: {positions}')
+    st.write(fig)
+
+
+
+@st.cache
+def fit_model(df):
+    # Filter out 
+    df = df[(df['G']>= 40) & (df['MP']>=10)]
+
+    # SVM Model
+    X = df[['2P','2PA','3P','3PA','DRB','ORB','AST','STL','BLK']]
+    y = df['Pos']
+
+    svm_pipe = make_pipeline(MinMaxScaler(),SVC(C=1.0, gamma=10.0))
+    
+    svm_pipe.fit(X, y)
+    
+    return svm_pipe
+    
+    
+
+def run_model(df):
+    
+    svm_pipe = fit_model(df)
+
+
+    # Inputs for model
+    row1_1, row1_2 = st.columns((1,1))
+    with row1_1:
+        two = st.number_input('2P')
+
+    with row1_2:
+        twoa = st.number_input('2PA')
+
+    row2_1, row2_2 = st.columns((1,1))
+    with row2_1:
+        three = st.number_input('3P')
+
+    with row2_2:
+        threea = st.number_input('3PA')
+
+    row3_1, row3_2 = st.columns((1,1))
+    with row3_1:
+        drb = st.number_input('DRB')
+
+    with row3_2:
+        orb = st.number_input('ORB')
+
+    row4_1, row4_2, row4_3 = st.columns((1,1,1))
+    with row4_1:
+        ast = st.number_input('AST')
+
+    with row4_2:
+        stl = st.number_input('STL')
+
+    with row4_3:
+        blk = st.number_input('BLK')
+
+
+
+    # Model prediction
+    player = [two,twoa,three,threea,drb,orb,ast,stl,blk]
+    pred = svm_pipe.predict([player])
+
+
+
+    # Exception
+    if (two <=0) and (twoa <=0) and (three <=0) and (threea <=0) and (drb <=0) and (orb <=0) and (ast <=0) and (stl <=0) and (blk <= 0):
+        pred = 0
+
+
+
+    # Button to execute model
+    if st.button('Predict Position'):
+        if pred == ['PG']:
+            st.success('Point Guard')
+        elif pred == ['SG']:
+            st.success('Shooting Guard')
+        elif pred == ['SF']:
+            st.success('Small Forward')
+        elif pred == ['PF']:
+            st.success('Power Forward')
+        elif pred == ['C']:
+            st.success('Center')
+        elif pred == 0:
+            st.error('Please enter some values')
 
 
 
@@ -87,145 +225,8 @@ def main():
             \nFor more details about this model, click [here](https://github.com/leftyamos/amost.github.io/blob/master/NBA_Classification.ipynb).''')
         
         run_model(df)
-     
-    
-
-@st.cache(allow_output_mutation=True)        
-def read_multi(file_list):
-    '''
-    take a list of files concat it together.
-    
-    '''
-    list_df = []
-    
-    for file in (file_list):
-        df = pd.read_csv(file, sep=',', header=0)
-        
-        df['Sea'] = file.split('.')[0]
-        
-        list_df.append(df)
-    
-    total_df = pd.concat(list_df, axis=0)
-    
-    return total_df
 
 
-
-def run_eda(df):
-    '''
-    Visualize data
-    '''
-    years = ['2010-2011', '2011-2012', '2012-2013', '2013-2014', '2014-2015',
-             '2015-2016', '2016-2017', '2017-2018', '2018-2019', '2019-2020']
-    
-    seasons = st.selectbox('Select season', years)
-    
-    fig = plt.figure(figsize=(10,5))
-    filter_season = df[df['Sea']==seasons]['Pos'].value_counts().sort_index().loc[['PG','SG','SF','PF','C']].plot(kind='bar')
-    plt.ylim(0,140)
-    plt.xlabel('Positions')
-    plt.ylabel('Players')
-    plt.title(f'Total no. of players during {seasons} season')
-    st.write(fig)
-    
-    row1_1, row1_2 = st.beta_columns((1,1))
-    with row1_1:
-        features = st.selectbox('Select feature', df.drop(['Rk','Pos','Player','Tm','GS','Sea'], axis=1).columns)
-        
-    with row1_2:
-        positions = st.selectbox('Select position', ['PG','SG','SF','PF','C'])
-    
-    fig = plt.figure(figsize=(3,3))
-    filter_fea_pos = sns.histplot(data=df[(df['Sea']==seasons) & (df['Pos']==positions)], x=features)
-    plt.title(f'Histogram \n{seasons}: {positions}')
-    st.write(fig)
-
-
-
-@st.cache
-def fit_model(df):
-    # Filter out 
-    df = df[(df['G']>= 40) & (df['MP']>=10)]
-
-    # SVM Model
-    X = df[['2P','2PA','3P','3PA','DRB','ORB','AST','STL','BLK']]
-    y = df['Pos']
-
-    svm_pipe = make_pipeline(MinMaxScaler(),SVC(C=1.0, gamma=10.0))
-    
-    svm_pipe.fit(X, y)
-    
-    return svm_pipe
-    
-    
-
-def run_model(df):
-    
-    svm_pipe = fit_model(df)
-
-
-    # Inputs for model
-    row1_1, row1_2 = st.beta_columns((1,1))
-    with row1_1:
-        two = st.number_input('2P')
-
-    with row1_2:
-        twoa = st.number_input('2PA')
-
-    row2_1, row2_2 = st.beta_columns((1,1))
-    with row2_1:
-        three = st.number_input('3P')
-
-    with row2_2:
-        threea = st.number_input('3PA')
-
-    row3_1, row3_2 = st.beta_columns((1,1))
-    with row3_1:
-        drb = st.number_input('DRB')
-
-    with row3_2:
-        orb = st.number_input('ORB')
-
-    row4_1, row4_2, row4_3 = st.beta_columns((1,1,1))
-    with row4_1:
-        ast = st.number_input('AST')
-
-    with row4_2:
-        stl = st.number_input('STL')
-
-    with row4_3:
-        blk = st.number_input('BLK')
-
-
-
-    # Model prediction
-    player = [two,twoa,three,threea,drb,orb,ast,stl,blk]
-    pred = svm_pipe.predict([player])
-
-
-
-    # Exception
-    if (two <=0) and (twoa <=0) and (three <=0) and (threea <=0) and (drb <=0) and (orb <=0) and (ast <=0) and (stl <=0) and (blk <= 0):
-        pred = 0
-
-
-
-    # Button to execute model
-    if st.button('Predict Position'):
-        if pred == ['PG']:
-            st.success('Point Guard')
-        elif pred == ['SG']:
-            st.success('Shooting Guard')
-        elif pred == ['SF']:
-            st.success('Small Forward')
-        elif pred == ['PF']:
-            st.success('Power Forward')
-        elif pred == ['C']:
-            st.success('Center')
-        elif pred == 0:
-            st.error('Please enter some values')
-
-
-            
+          
 if __name__ == "__main__":
     main()
